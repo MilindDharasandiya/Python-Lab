c3201f584b8c6703825a9810be2fd36877af9bab
MilindDharasandiya <24bch173@spt.pdpu.ac.in>
2025-04-21 20:44:14 +0530

    Delete question 17.py

--- question 17.py
+++
@@ -1,5 +0,0 @@
-side = int(input("ENTER THE SIDE OF THE SQUARE : "))
-area = side * side
-perimeter = 4 * side
-print("THE VALUE OF PERIMETER OF THE SQUARE : " , perimeter)
-print("THE VALUE OF AREA OF THE SQUARE : " , area)
